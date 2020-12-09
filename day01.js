2636411c576ad5e2a4e35b1b43facc393239df33
Jip <jhp.moors@gmail.com>
2020-12-09 22:24:31 +0100

    Day 1: Part 2

--- day01.js
+++ day01.js
@@ -3,10 +3,18 @@ const data = fs.readFileSync( "day01.list.txt", "utf-8" );
 
 const rows = data.split( "\n" ).filter( ( x ) => x ).map( ( row ) => parseInt( row, 10 ) );
 
-const fuel = rows
-	.map( ( row ) => Math.floor( row / 3 ) - 2 )
+function getFuel( amount ) {
+	let fuel = Math.max( 0, Math.floor( amount / 3 ) - 2 );
+	if ( fuel > 0 ) {
+		fuel += getFuel( fuel );
+	}
+	return fuel;
+}
+
+const totalFuel = rows
+	.map( getFuel )
 	.reduce( ( acc, current ) => {
 		return acc + current;
 	}, 0 );
 
-console.log( fuel );
+console.log( totalFuel );
